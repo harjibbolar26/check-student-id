01cc39f08e0a79cbf1030387222c0e5355b3465d
Jibola Paul <harjibbolar26@gmail.com>
2023-08-19 13:50:15 +0100

    new comit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const list = document.querySelector(".list");
+// const subMenuList = document.querySelector(".sub-menu-list")
+
+// list.addEventListener("mouseover", () => {
+//     subMenuList.style.visibility = "visible"
+//     // alert("hi")
+// })
+
+// list.addEventListener("mouseout", () => {
+//     subMenuList.style.visibility = "hidden"
+// })
